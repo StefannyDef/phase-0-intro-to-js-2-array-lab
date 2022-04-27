6c1e643edcaf8b1572bd1f35d03a2e0e2a59755c
Stefanny Al <stefannydefreitas@gmail.com>
2022-04-27 13:26:01 -0400

    commit tree 5dda5ab9c6ad0dc6df2bf48b481df18ff3060a3b

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+
+let cats=['Milo', 'Otis', 'Garfield']; 
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift()
+  }
+
+  function destructivelyRemoveLastCat(name){
+      cats.pop()
+  }
+
+function removeLastCat(){
+    let copyOfCats = [...cats]
+    copyOfCats.pop();
+return copyOfCats
+
+}
+
+
+function removeFirstCat(){
+    let copyOfCats = [...cats]
+    copyOfCats.shift();
+    return copyOfCats
+}
+
+
+    function prependCat(name){
+    let copyOfCats = [name, ...cats]
+    copyOfCats.unshift();
+    return copyOfCats
+    }
+
+    function appendCat(name){
+        let copyOfCats = [... cats, name]
+       copyOfCats.push();
+       return copyOfCats
+    
+    }
